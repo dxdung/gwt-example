21b5379f663f5650da2f1df1c2c86d8f847512e7
Dung Dinh <dxdung@tma.com.vn>
2017-12-29 13:37:44 +0700

    modify react component build path

--- react-component/webpack.config.js
+++ react-component/webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   entry: './index.js',
   output: {
-    path: './dist',
+    path: './../war/lib',
     filename: 'react-component-example.js',
     library: 'reactComponentExample',
     libraryTarget: 'umd',
